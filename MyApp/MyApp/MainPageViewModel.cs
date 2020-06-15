67c4313db5f0a4032618e79eadf71db1dd73b43f
802622 <54591437+802622@users.noreply.github.com>
2020-06-14 23:28:55 -0700

    tried covert to string, failed

---
+++ MyApp/MyApp/MainPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace MyAppMainPage
+
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() => { TheNote = string.Empty; });
+            SaveCommand = new Command(() =>
+            {
+                System.Diagnostics.Debug.WriteLine("allNotes is: " + AllNotes);
+                System.Diagnostics.Debug.WriteLine("TheNote is: " +  TheNote);
+                AllNotes.Add(TheNote);
+                //AllNotes += TheNote;
+
+                TheNote = string.Empty;
+            });
+        }
+        public ObservableCollection<string> AllNotes { get; set; }//change to string?\
+        AllNotes.Add("heyHeyHEY");
+        //public string AllNotes = "zsfasdf";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+        public string TheNote
+        {
+            //Makes theNote change in both the view model and in the code!
+            get => theNote;
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand { get; }
+        public Command EraseCommand { get; }
+        
+       
+
+    }
+}
